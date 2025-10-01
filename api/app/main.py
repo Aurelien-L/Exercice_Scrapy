80afdfc303b02e41bab6da8a5ca177023a6a59d6
aurelienL <aurelienleva.pro@gmail.com>
2025-10-01 10:30:48 +0200

    Redirection page d'accueil

--- api/app/main.py
+++ api/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-from app import models, database
+from fastapi.responses import RedirectResponse
 from app.routers import books, categories
 
 
-app = FastAPI(title="Scraped books API")
+app = FastAPI(title="Scraped books API", version="1.0.0")
+
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/docs")
 
 app.include_router(books.router)
 app.include_router(categories.router)
